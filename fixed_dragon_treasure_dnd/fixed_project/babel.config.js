4b447667b6c26e46c406973bbfed5c0549a0df26
dlcjstjd92-code <dlcjstjd92@gmail.com>
2026-03-22 19:19:41 +0900

    Delete fixed_dragon_treasure_dnd/fixed_project directory

--- fixed_dragon_treasure_dnd/fixed_project/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-    // NOTE: react-native-worklets/plugin is automatically added by babel-preset-expo
-    // when react-native-worklets is installed. Do NOT add it manually.
-  };
-};
